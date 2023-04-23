7a4c304f7c9b2d8375616e9e89618dcd28d04303
makwe <makwe@MAHALEFA-PC>
2023-04-23 19:44:11 +0200

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recipe recipe = new Recipe();
+            while (true)
+            {
+                
+                
+                Console.WriteLine("What would you like to do? \n"
+                                + "1) Enter a new recipe. \n"
+                                + "2) Display the current recipe. \n"
+                                + "3) Scale the recipe. \n"
+                                + "4) Reset the recipe quantities. \n"
+                                + "5) Clear all data. \n"
+                                + "6) Exit the program.");
+                string input = Console.ReadLine();
+                int choice = int.Parse(input);
+
+                switch (choice)
+                {
+                    case 1:
+                        recipe.EnterRecipe();
+                        break;
+                    case 2:
+                        recipe.DisplayRecipe();
+                        break;
+                    case 3:
+                        recipe.ScaleRecipe();
+                        break;
+                    case 4:
+                        recipe.ResetQuantities();
+                        break;
+                    case 5:
+                        recipe.ClearData();
+                        break;
+                    case 6:
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            }
+        }
+    }
+
+}
---
+++ ConsoleApp7/Recipe.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Recipe
+    {
+        private string[] ingredients;
+        private double[] quantities;
+        private string[] units;
+        private string[] steps;
+
+        public void EnterRecipe()
+        {
+            Console.WriteLine("How many ingredients are in the recipe?");
+            int numIngredients = int.Parse(Console.ReadLine());
+
+            ingredients = new string[numIngredients];
+            quantities = new double[numIngredients];
+            units = new string[numIngredients];
+
+            for (int i = 0; i < numIngredients; i++)
+            {
+                Console.WriteLine($"Enter the name of ingredient {i + 1}:");
+                ingredients[i] = Console.ReadLine();
+
+                Console.WriteLine($"Enter the quantity of ingredient {i + 1}:");
+                quantities[i] = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Enter the unit of measurement for ingredient {i + 1}:");
+                units[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("How many steps are in the recipe?");
+            int numSteps = int.Parse(Console.ReadLine());
+
+            steps = new string[numSteps];
+
+            for (int i = 0; i < numSteps; i++)
+            {
+                Console.WriteLine($"Enter step {i + 1}:");
+                steps[i] = Console.ReadLine();
+            }
+        }
+
+        public void DisplayRecipe()
+        {
+            Console.WriteLine("Ingredients:");
+            for (int i = 0; i < ingredients.Length; i++)
+            {
+                Console.WriteLine($"{quantities[i]} {units[i]} of {ingredients[i]}");
+            }
+
+            Console.WriteLine("Steps:");
+            for (int i = 0; i < steps.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {steps[i]}");
+            }
+        }
+
+        public void ScaleRecipe()
+        {
+            Console.WriteLine("Enter the scale factor (0.5, 2, or 3):");
+            double scaleFactor = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quantities.Length; i++)
+            {
+                quantities[i] *= scaleFactor;
+            }
+        }
+
+        public void ResetQuantities()
+        {
+            
+            for (int i = 0; i < quantities.Length; i++)
+            {
+                quantities[i] = 0;
+            }
+            
+        }
+
+        public void ClearData()
+        {
+            ingredients = null;
+            quantities = null;
+            units = null;
+            steps = null;
+        }
+    }
+}
